7e75eef0ab166a9d698353d19443862fb813a634
madhav-singla <madhav0430.becse24@chitkara.edu.in>
2025-02-11 14:47:06 +0530

    Solution to Determine Eligibility for Voting

---
+++ Determine Eligibility for Voting@@6788dd63490637cfc44e1b99/code.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int a;
+    scanf("%d ", &a);
+
+    // Check if it's a valid triangle
+    if (a>=18) {
+printf(Eligible);
+    } else {
+    printf("Not Eligible")
+    }
+
+    return 0;
+}
