8b6fc5247659e384e851c6e496d24096ebd3c5ba
madhav-singla <madhav0430.becse24@chitkara.edu.in>
2025-02-11 11:52:09 +0530

    Solution to Logical OR Operation

---
+++ Logical OR Operation@@67882a731b4392d8fc1577dd/code.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int a, b;
+    scanf("%d %d", &a, &b);  // Read two integers
+
+    if (a > 0|| b>0) {
+        printf("True\n");  // Print "True" if a is greater than b
+    } else {
+        printf("False\n");  // Print "False" if a is not greater than b (semicolon added)
+    }
+
+    return 0;
+}
