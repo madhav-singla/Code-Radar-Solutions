c9f42e5264628fdfb5ae1d4ae31f54c90403f908
madhav-singla <madhav0430.becse24@chitkara.edu.in>
2025-02-12 10:08:58 +0530

    Solution to Sum of First N Natural Numbers

--- Sum of First N Natural Numbers@@676c4885c18c9c17a0f50667/code.c
+++ Sum of First N Natural Numbers@@676c4885c18c9c17a0f50667/code.c
@@ -3,15 +3,14 @@
 int main()
 {
     int n, m;
-    printf("Enter the number of rows>>>");
+
     scanf("%d", &n);
-    printf("Enter the number of columns>>>");
-    scanf("%d", &m);
+
 int sum = 0;
     for (int i = 1; i <= n; i++) {
 sum += i;
-        printf("%d " , sum);
     }
+        printf("%d " , sum);
 
     return 0;
 }
