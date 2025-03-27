8791f6c2291e6e532c056d578737c0132a72dbe1
madhav-singla <madhav0430.becse24@chitkara.edu.in>
2025-03-27 14:19:51 +0530

    Solution to Caesar Cipher Encryption

---
+++ Caesar Cipher Encryption@@67ca7acd1fe08d7207c253c3/code.c
@@ -0,0 +1,18 @@
+void caesarCipher(char message, int shift, char encrypted)
+{
+    for (int i = 0; i < count; i++)
+    {
+        char ch = message[i];
+        if (isupper(ch))
+        {
+            encrypted[i] = ((ch - 'A' + shift)%26) + 'A';
+        }
+        if (islower(ch))
+        {
+            encrypted[i] = ((ch - 'a' + shift)%26) + 'a';
+        }
+        else{
+            encrypted[i] = ch;
+        }
+    }
+}
