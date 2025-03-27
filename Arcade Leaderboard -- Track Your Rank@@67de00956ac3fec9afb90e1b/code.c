85ac40c23ac1c726b3bc7b228437b8084aa4e572
madhav-singla <madhav0430.becse24@chitkara.edu.in>
2025-03-27 11:40:12 +0530

    Solution to Arcade Leaderboard -- Track Your Rank

--- Arcade Leaderboard -- Track Your Rank@@67de00956ac3fec9afb90e1b/code.c
+++ Arcade Leaderboard -- Track Your Rank@@67de00956ac3fec9afb90e1b/code.c
@@ -1,25 +1,24 @@
 #include <stdio.h>
 
 void trackPlayerRanks(int ranked[], int n, int player[], int m, int result[]) {
-    // Step 1: Create a list of unique scores
-    int denseRank[200000]; // To store unique scores
-    int uniqueCount = 0; // Count of unique scores
     
-    denseRank[uniqueCount++] = ranked[0]; // First score always gets included
+    int denseRank[200000];
+    int uniqueCount = 0; 
+    
+    denseRank[uniqueCount++] = ranked[0];
 
     for (int i = 1; i < n; i++) {
-        if (ranked[i] != ranked[i - 1]) { // Skip duplicates
+        if (ranked[i] != ranked[i - 1]) { 
             denseRank[uniqueCount++] = ranked[i];
         }
     }
 
-    // Step 2: Find player's rank efficiently
-    int j = uniqueCount - 1; // Start from the lowest rank
+    int j = uniqueCount - 1;
 
     for (int i = 0; i < m; i++) {
         while (j >= 0 && player[i] >= denseRank[j]) { 
-            j--; // Move up the ranks if player's score is higher
+            j--;
         }
-        result[i] = j + 2; // Rank is (j+1), but since index starts at 0, add 1 more
-    }
+        result[i] = j + 2; 
+        }
 }
